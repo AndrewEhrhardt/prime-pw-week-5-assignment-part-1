8aeb39fd25d6160331495126186a5fb09dd4cf93
AndrewEhrhardt <andyehrhardt@gmail.com>
2021-08-09 15:06:09 -0500

    completed assignment

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -9,23 +9,31 @@ console.log('***** Object Practice *****')
   - Also give it an array 'favThreeFoods'
 */
 const me = {
-  // TODO - add properties here
+  firstName: "Andy",
+  lastName: "Ehrhardt",
+  hasSiblings: true,
+  shoeCount: 5,
+  favThreeFoods: ["Pizza", "Ramen", "Sushi"]
 };
 console.log('A little about me:', me);
 
 /* 2. Accessing object properties.
-  - Create a variable called fullName 
-  - Use the firstName & lastName properties of the object you 
-    created above to set its value. 
+  - Create a variable called fullName
+  - Use the firstName & lastName properties of the object you
+    created above to set its value.
   - Console.log fullName
 */
-
-
+let fullName = me.firstName + " " + me.lastName;
+console.log(fullName);
 
 /* 3. Nested arrays
   - Console.log your first favorite food
-  - Console.log your last favorite food 
+  - Console.log your last favorite food
 */
+console.log(me.favThreeFoods[0]);
+console.log(me.favThreeFoods[me.favThreeFoods.length-1]);
+//For the console.log above, I found the length of favThreeFoods to access the
+//last item, in case the there were ever more than 3 items contained within the array
 
 
 
@@ -33,9 +41,11 @@ console.log('A little about me:', me);
   - Console.log your current shoe count.
   - Guess what?! You just got a new pair of shoes!!!
   - Set your object's shoeCount to be what it was plus 1
-  - Console.log your updated shoe count. 
+  - Console.log your updated shoe count.
 */
-
+console.log(me.shoeCount);
+me.shoeCount++//Adding one to the number assigned to shoeCount
+console.log(me.shoeCount);
 
 /* 5. Add a new property to an existing object.
   - Add a 'favoriteColor' to your existing object
@@ -43,3 +53,5 @@ console.log('A little about me:', me);
   - Cool huh? It works, even though it wasn't there before
   - Console.log your object
 */
+me.favoriteColor = "Red";
+console.log(me.favoriteColor);
